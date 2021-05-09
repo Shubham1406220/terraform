13b23afcaaee1b6b1b7c10f63cd31ae5967e517f
Shubham1406220 <76969039+Shubham1406220@users.noreply.github.com>
2021-05-09 20:02:24 +0530

    Update testing.tf

--- testing.tf
+++ testing.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
   secret_key = "iEKduJpZG5gWghVTKGLRcAN6Dy4TDglC/e1gEBfR"
-  access_key = "AKIAQQKMOHRB4GCAEIES"
+  access_key = "AKIAQQKMOHRB4GCAEIES""
  }
 resource "aws_instance" "testing" {
   ami = "ami-010aff33ed5991201"
